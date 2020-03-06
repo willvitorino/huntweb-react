40418222f7c3fd30119a41f97eb3eba2680ebb76
William Vitorino <williamvitorino3@gmail.com>
2020-03-06 09:26:00 -0300

    url da base adicionada

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Main from './pages/main'
 import Products from './pages/products'
 
+const baseUrl = '/huntweb-react/'
+
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Main} />
-      <Route path="/products/:id" component={Products} />
+      <Route exact path={baseUrl} component={Main} />
+      <Route path={baseUrl+"products/:id"} component={Products} />
     </Switch>
   </BrowserRouter>
 )
