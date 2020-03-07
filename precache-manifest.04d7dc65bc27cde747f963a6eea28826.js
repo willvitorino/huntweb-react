530037407780b39d6222fc9a55c1787d5d45eaf3
William Vitorino <williamvitorino3@gmail.com>
2020-03-07 15:15:56 -0300

    7430b35

---
+++ precache-manifest.04d7dc65bc27cde747f963a6eea28826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dc61d34619f790530f17cc8729cc75",
+    "url": "/huntweb-react/index.html"
+  },
+  {
+    "revision": "fa83382af210d26d6c68",
+    "url": "/huntweb-react/static/css/main.0c2e6f8d.chunk.css"
+  },
+  {
+    "revision": "e37e54c6e757526f099d",
+    "url": "/huntweb-react/static/js/2.f0c76f80.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/huntweb-react/static/js/2.f0c76f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa83382af210d26d6c68",
+    "url": "/huntweb-react/static/js/main.376ff5d3.chunk.js"
+  },
+  {
+    "revision": "890aef66b4089dd1ea56",
+    "url": "/huntweb-react/static/js/runtime-main.8249d8d6.js"
+  }
+]);
